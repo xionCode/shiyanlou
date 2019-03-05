76d956e1753bf9dc9704e937dda678ec6f62ad4b
xoncode <xiontime1994@gmail.com>
2019-03-05 16:31:59 +0800

    111

--- callenge3_2/challenge3_2.py
+++ callenge3_2/challenge3_2.py
@@ -4,23 +4,29 @@ from openpyxl import load_workbook # 可以用来载入已有数据表格
 from openpyxl import Workbook # 可以用来处理新的数据表格
 import datetime # 可以用来处理时间相关的数据
 
+wb = load_workbook('courses.xlsx')
+students_sheet = wb['students']
+time_sheet = wb['time']
+
 def combine():
     '''
     该函数可以用来处理原数据文件：
     1. 合并表格并写入的 combine 表中
     2. 保存原数据文件
     '''
-    wb = load_workbook('courses.xlsx')
-    studentsSheet = wb['students']
-    combineSheet = wb.copy_worksheet(studentsSheet)
-    combineSheet.title = 'combine'
+    combine_sheet = wb.copy_worksheet(students_sheet)
+    combine_sheet.title = 'combine'
+    combine_sheet['D1'] = '学习时间'
+    for row in combine_sheet.iter_rows(2):
+        course_name = row[1].value
+        cell_row = row[1].row
 
+        for row in time_sheet.iter_rows(2):
+            if row[1].value == course_name:
+                combine_sheet.cell(row=cell_row, column=4,value=row[2].value)
+                break
     
-    wb.save('courses2.xlsx')
-
-
-
-
+    wb.save('courses4.xlsx')
 
 def split():
     '''
