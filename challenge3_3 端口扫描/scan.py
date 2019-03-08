5634473916535aae5b442abf549b01f04673dd3c
xoncode <xiontime1994@gmail.com>
2019-03-08 15:12:22 +0800

    优化

--- challenge3_3 端口扫描/scan.py
+++ challenge3_3 端口扫描/scan.py
@@ -1,6 +1,28 @@
 from socket import socket
 import sys, re
 
+def get_argv():
+    argv_list = sys.argv[1:]
+    try:
+        host_index = argv_list.index('--host')
+        host_temp = argv_list[host_index + 1]
+        port_index = argv_list.index('--port')
+        port_temp = argv_list[port_index + 1]
+
+        if re.match('^(\d{1,3}\.){3}\d{1,3}$', host_temp):
+            host_ip = host_temp
+        
+        if re.match('\d+(-\d+)?', port_temp):
+            port_list_num = [int(x) for x in port_temp.split('-')]
+            if len(port_list_num) == 1:
+                return host_ip, port_list_num
+            else:
+                port_list = [x for x in range(port_list_num[0], port_list_num[1]+1)]
+                return host_ip, port_list
+    except (ValueError, IndexError):
+        print('Parameter Error')
+        exit()
+
 def scan_port(host_ip, port_list):
     for port in port_list:
         s = socket()
@@ -11,24 +33,8 @@ def scan_port(host_ip, port_list):
             print(port, 'closed')
         s.close()
 
-
-
 if __name__ == '__main__':
-    if len(sys.argv) == 5:
-        if sys.argv[1] == '--host' and sys.argv[3] == '--port':
-            if re.match('^(\d{1,3}\.){3}\d{1,3}$', sys.argv[2]) and re.match('\d+(-\d+)?', sys.argv[4]):
-                host_ip = sys.argv[2]
-                port_list_num = [int(x) for x in sys.argv[4].split('-')]
-                if len(port_list_num) == 1:
-                    scan_port(host_ip, port_list_num)
-                else:
-                    port_list = [x for x in range(port_list_num[0], port_list_num[1]+1)]
-                    scan_port(host_ip, port_list)
-            else:
-                print('Parameter Error1')
-        else:
-            print('Parameter Error2')
-    else:
-        print('Parameter Error3')
+    host_ip, port_list = get_argv()
+    scan_port(host_ip, port_list)
 
 
